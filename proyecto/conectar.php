0be21db354ba774430dfd60b53e2368e164e4003
marco-29 <126988064+marco-29@users.noreply.github.com>
2024-11-06 10:47:04 -0600

    0.1.0 Conexón

--- proyecto/conectar.php
+++ proyecto/conectar.php
@@ -3,26 +3,26 @@
 		$servername = 'localhost';
 		$database = 'panecito';
 		$username = 'root';
-		$password = '';
+		$password = 'root';
 
 		if (!($conn = mysqli_connect($servername, $username, $password)))
 		{
-			print("Error al conectarse a la Base de datos. <br>");
+			echo "Error al conectarse a la Base de datos. <br>";
 			exit();
 		}
 		else
 		{
-			print("Conexion exitosa. <br>");
+			echo "Conexion exitosa. <br>";
 		}
 
 		if (!mysqli_select_db($conn, $database))
 		{
-			print("Error al seleccionar la base de datos. <br>");
+			echo "Error al seleccionar la base de datos. <br>";
 			exit();
 		}
 		else
 		{
-			print("Conexion exitosa a la base de datos [$database]. <br>");
+			echo "Conexion exitosa a la base de datos [$database]. <br>";
 		}
 
 		return $conn;
